bc007a94cdee839cdff9765a6687c937dfc3cfb8
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2015-11-03 12:33:58 +0000

    Finished basic site layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 
+var accordionClosed = true;
+
+$(function(){
+	$(".accordion").accordion({
+		collapsible: true,
+		active: false,
+		beforeActivate: function(event, ui) {
+			if(accordionClosed){
+				$(".accordionToggle").html("-");
+			}else{
+				$(".accordionToggle").html("+");
+			}
+
+			accordionClosed = !accordionClosed;
+		}
+	});
+});
